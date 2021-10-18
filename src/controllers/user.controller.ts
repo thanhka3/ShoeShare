5760b305f541b923609b560d19a931a881d2e342
Thành <thanh.ha@itbeesolutions.vn>
2021-10-18 12:14:48 +0700

    new code2

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,4 +1,4 @@
-import { User } from "../models/User.model";
+import { User } from "../models/user.model";
 import BaseResponse from "../models/response/model.BaseResponse";
 import * as userCredentialsController from './userCredentials.controller';
 
