4c955a1c3390ab5a8e9240c719ae6454e9a245ae
Thành <thanh.ha@itbeesolutions.vn>
2021-10-19 09:29:21 +0700

    new code

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -8,25 +8,25 @@ export const createUser = async (req: any, res: any) => {
         baseResponse.setError('Body request is Required');
         return res.status(401).json(baseResponse.response);
     }
-    await User.find({ email: req.body.email, isDeleted: false })
-        .then((isEmailExisted: any) => {
+    await User.findOne({ email: req.body.email, isDeleted: false })
+        .then((isEmailExisted) => {
             if (isEmailExisted) {
                 baseResponse.setError('email đã tồn tại');
                 return res.status(404).json(baseResponse.response);
             }
         })
-        .catch((err: any) => {
+        .catch((err) => {
             console.log(err)
         });
 
-    await User.find({ phone: req.body.phone, isDeleted: false })
-        .then((isPhoneExisted: any) => {
+    await User.findOne({ phone: req.body.phone, isDeleted: false })
+        .then((isPhoneExisted) => {
             if (isPhoneExisted) {
                 baseResponse.setError('số điện thoại đã tồn tại');
                 return res.status(404).json(baseResponse.response);
             }
         })
-        .catch((err: any) => {
+        .catch((err) => {
             console.log(err)
         });
 
@@ -41,10 +41,12 @@ export const createUser = async (req: any, res: any) => {
     await user.save();
 
     await User.findOne({ email: user.email, isDeleted: false, phone: user.phone })
-        .then((userCreated: any) => {
+        .then((userCreated) => {
+            const result = userCredentialsController.createUserCredentials(userCreated._id, req.body.password);
+            console.log(result);
             res.json(userCreated);
         })
-        .catch((err: any) => console.log(err));
+        .catch((err) => console.log(err));
     // if (userCreated) {
     //     userCredentialsController.createUserCredentials(userCreated.id, req.body.password);
     // }
--- src/controllers/userCredentials.controller.ts
+++ src/controllers/userCredentials.controller.ts
@@ -1,12 +1,13 @@
 import { UserCredentials } from "../models/userCredentials.model";
 
-export const createUserCredentials = async (userId: string, password: string): Promise<any> => {
+export const createUserCredentials = async (userId: string, password: string) => {
     var userCredentials = new UserCredentials();
     const userCredential = {
         userId: userId,
         password: password
     }
 
-    const res = await userCredentials.create(userCredential);
-    return res;
+    await UserCredentials.create(userCredential)
+        .then(res => { return res })
+        .catch(err => console.log(err));
 }
